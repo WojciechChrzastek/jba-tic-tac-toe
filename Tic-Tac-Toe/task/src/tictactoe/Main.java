5a48abdc27a1e1fb63fd86464fe30f315f3d5d62
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-06 18:54:39 +0200

    Print field from user input. Complete Stage #2

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,25 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X\nO X O\nX X O");
+        printField(takeInput());
+    }
+
+    static void printField(char[] input) {
+        String line = "| %s %s %s |\n";
+
+        System.out.println("---------");
+        System.out.printf(line, input[0], input[1], input[2]);
+        System.out.printf(line, input[3], input[4], input[5]);
+        System.out.printf(line, input[6], input[7], input[8]);
+        System.out.println("---------");
+    }
+
+    static char[] takeInput() {
+        System.out.print("Enter cells: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().toCharArray();
     }
 }
